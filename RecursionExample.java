2b12b869606734c070aa2f8fa679ba3ff5552e1d
Hodge-zf <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-02 08:36:44 -0600

    Correct

--- RecursionExample.java
+++ RecursionExample.java
@@ -4,7 +4,7 @@ public class RecursionExample{
     System.out.println(recursion.doFactorial(5));
   }
   public int doFactorial(int n){
-      int answer = (n)*(n - 1)*(n - 2)*(n - 3)*(n - 4);
-        return answer;
+    if(n==1) return 1;
+    return n = n*doFactorial(n-1);
   }
 }
