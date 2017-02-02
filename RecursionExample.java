ee3a0ca5e56427bee34b67134ad7d45d7bd1248c
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-02 08:03:59 -0600

    recursion

---
+++ RecursionExample.java
@@ -0,0 +1,10 @@
+public class RecursionExample{
+  public static void main(String[] args){
+    RecursionExample recursion = newRecursionExample();
+    System.out.println(recursion.doFactorial(5));
+  }
+  public int doFactorial(int n){
+      //doFactorial -> Goal -no for/while or do/while loop
+  }
+  }
+}
