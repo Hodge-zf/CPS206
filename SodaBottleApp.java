4e31a502ec0bd0a9380f39ca95e19b01382c0f49
Randy Hodge <xx.skcs@gmail.com>
2017-02-07 11:38:10 -0600

    Add files via upload

---
+++ SodaBottleApp.java
@@ -0,0 +1,10 @@
+package edu.jalc.inclass.bottle;
+
+public class SodaBottleApp{
+  public static void main(String[] args){
+
+    SodaBottle sodaBottle = new SodaBottle();
+    System.out.println(sodaBottle.getCapacity());
+
+  }
+}
