61028c94c4dc9ec08076bc95e0675a9e91fb0845
Hodge-zf <xx.skcs@gmail.com>
2017-02-10 08:47:44 -0600

    Liquid cleaned up & -> into Bottle, import needs fixed

--- edu/jalc/inclass/bottle/SodaBottle.java
+++ edu/jalc/inclass/bottle/SodaBottle.java
@@ -1,10 +1,24 @@
 package edu.jalc.inclass.bottle;
+import edu.jalc.inclass.liquid.Soda;
 
 public class SodaBottle{
-  
-  private final double capacity = 20;
+
+  private final double capacity;
+  private Soda soda;
+
+  SodaBottle(double capacity, Soda soda){
+    this.capacity = capacity;
+    this.setSoda(soda);
+  }
 
   public double getCapacity(){
     return capacity;
     }
+  Soda getSoda(){
+    return soda;
   }
+  SodaBottle setSoda(Soda soda){
+    this.soda = soda;
+    return this;
+  }
+}
--- edu/jalc/inclass/liquid/Soda.java
+++ edu/jalc/inclass/liquid/Soda.java
@@ -1,21 +1,21 @@
 package edu.jalc.inclass.liquid;
 
-public class Soda {
+public class Soda{
 
     private double volume;
 
-    public Soda(){
-      this.setVolume(0.0);
-    }
-
     public Soda(double volume){
-      this.setVolume(volume);
+      setVolume(volume);
     }
-    public double getVolume(){
-      return this.volume;
+    public Soda(){
+      setVolume(0.0);
     }
     public Soda setVolume(double volume){
       this.volume = volume;
       return this;
     }
+    public double getVolume(){
+      return volume;
+    }
+
 }
