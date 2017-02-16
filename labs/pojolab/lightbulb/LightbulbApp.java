63d10b80c1f92e4dbeac2cba84d5a551086fb589
Hodge-zf <xx.skcs@gmail.com>
2017-02-15 23:24:19 -0600

    directory fix

--- labs/pojolab/lightbulb/LightbulbApp.java
+++
@@ -1,8 +0,0 @@
-//package labs.pojolab.lightbulb;
-
-public class LightbulbApp{
-  public static void main(String[] args) throws Exception{
-    Lightbulb lightBulb = new Lightbulb(120);
-    System.out.println("Bulb Wattage: "+lightBulb.getWattage()+'W');
-  }
-}
