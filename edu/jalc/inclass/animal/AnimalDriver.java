ad47113d11af04b7198fb60661dad3d2f970c1b0
Hodge-zf <xx.skcs@gmail.com>
2017-02-24 08:36:05 -0600

    created package animal

---
+++ edu/jalc/inclass/animal/AnimalDriver.java
@@ -0,0 +1,14 @@
+package edu.jalc.inclass.animal;
+
+public class AnimalDriver{
+
+  //Animal animal = Animal("growl");
+
+public static void main(String[] args){
+  animalSays();
+}
+
+public static void animalSays(String animal){
+  System.out.println(animal);
+  }
+}
