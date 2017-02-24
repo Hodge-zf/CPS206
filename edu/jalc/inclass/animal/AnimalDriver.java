5cb7fa2b333e1effc753fe768c3d2f1c9f572f3b
Hodge-zf <xx.skcs@gmail.com>
2017-02-24 11:04:30 -0600

    Animal Driver runs + repo cleaned

--- edu/jalc/inclass/animal/AnimalDriver.java
+++ edu/jalc/inclass/animal/AnimalDriver.java
@@ -1,18 +1,22 @@
 package edu.jalc.inclass.animal;
+
 import java.util.ArrayList;
+
 public class AnimalDriver{
 
-  public static void animalSays(Animal animal){
+  public static void animalSays(ArrayList<Animal> animal){
     System.out.println(animal);
   }
 
-  public void main(String[] args){
+  public static void main(String[] args){
+  
     ArrayList<Animal> animals = new ArrayList<>();
+    
     animals.add(new Dog("bark","growl","whimper"));
     animals.add(new Cat("meow","purr","growl"));
 
     for(Animal animal : animals){
-    AnimalDriver.animalSays(animals);
+       AnimalDriver.animalSays(animals);
     }
   }
 }
