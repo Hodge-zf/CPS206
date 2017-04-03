1931f365eb0898e242c00fb5a8816486f390292f
Hodge-zf <xx.skcs@gmail.com>
2017-04-03 08:45:47 -0500

    Anonymous classes & Lambdas

---
+++ examples/ThreadExample/DriverLambda.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+public class DriverLambda{
+
+  static int count = 0;
+  public static void main(String[] args){
+
+    ArrayList<Thread> threads = new ArrayList<>();
+
+    for(int i = 0; i < 10; i++){
+      threads.add(new Thread(() -> {
+        int myNumber = count;
+        count++;
+        try{
+          Thread.sleep((int)(Math.random()+10000));
+          System.out.println("Thread-"+myNumber+" running");
+        } catch (Exception e){
+          System.out.println("Thread-"+myNumber+" interrupted");
+    }
+  }));
+    }
+
+    System.out.println(threads);
+
+    threads.stream().forEach((thread) -> thread.start());
+
+    threads.sort((a, b) -> b.getName().compareTo(a.getName()));
+
+    List<String> names = threads.stream()
+      .filter((thread) -> thread.getName().equals("Thread-0"))
+      .map((thread) -> thread.getName())
+      .collect(Collectors.toList());
+    System.out.println(threads);
+  }
+}
---
+++ examples/ThreadExample/README.md
@@ -0,0 +1,4 @@
+# Randy Hodge
+## JALC Spring 2017
+
+First look at Anonymous Classes and Lambda expressions
